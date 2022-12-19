6b567f4a45bf2c7aeecfd0463356874d31fb0da9
Kostiantyn Zaremba <avadonkos@gmail.com>
2022-12-19 13:15:45 +0200

    task-09 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const formEl = document.querySelector('.login-form');
+const inputEmailEl = document.querySelector('input[name="email"]');
+const inputPasswordEl = document.querySelector('input[name="password"]');
 
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (inputEmailEl.value === '' || inputPasswordEl.value === '') {
+    alert('всі поля повинні бути заповнені');
+  } else {
+    
+  }
+});
